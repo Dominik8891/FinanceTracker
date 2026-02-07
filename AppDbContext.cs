c383872b5575cd222882723acc299babe404f2ff
Dominik8891 <dominikkusber33@gmail.com>
2026-02-07 18:18:51 +0100

    Initial commit: Grundgerüst mit Datenbank und Tracker-Seite

---
+++ AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FinanceTracker.Models;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    // Das wird unsere Tabelle in der Datenbank
+    public DbSet<Transaction> Transactions { get; set; }
+}
